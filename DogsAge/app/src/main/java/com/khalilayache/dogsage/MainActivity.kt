d748a822ac4aae01e0fa57f3bbee82d8b8b13c8f
Khalil Ayache <khalilayache@gmail.com>
2017-04-21 21:50:49 -0300

    Section 9, Class 58

--- DogsAge/app/src/main/java/com/khalilayache/dogsage/MainActivity.kt
+++ DogsAge/app/src/main/java/com/khalilayache/dogsage/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.khalilayache.dogsage
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val dogsAgeEditText: EditText by lazy { findViewById(R.id.dogsAgeEditText) as EditText }
+    private val calculateButton: Button by lazy { findViewById(R.id.calculateButton) as Button }
+    private val humanAgeTextView: TextView by lazy { findViewById(R.id.humanAgeTextView) as TextView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        calculateButton.setOnClickListener {
+            if (dogsAgeEditText.text.isNullOrEmpty()) {
+                Toast.makeText(this, getString(R.string.inform_dog_age), Toast.LENGTH_LONG).show()
+            } else {
+                val dogAge =  dogsAgeEditText.text.toString().toInt()
+                val humanAge = dogAge.times(7)
+                hideKeyboard()
+                humanAgeTextView.text = getString(R.string.dog_human_age,humanAge)
+            }
+        }
+    }
+
+    private fun hideKeyboard() {
+        try{
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromInputMethod(calculateButton.windowToken,0)
+        } catch (e: Exception){
+            Log.e("MainActivity", "EXCEPTION THROWS")
+        }
     }
 }
+
+
+
