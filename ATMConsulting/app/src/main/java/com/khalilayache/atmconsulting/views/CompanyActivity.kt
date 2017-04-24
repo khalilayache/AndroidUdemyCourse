21a0824c3d284ed028e5895e9c911d6dfa1d62b2
Khalil Ayache <khalilayache@gmail.com>
2017-04-24 19:06:01 -0300

    Section 10, Class 71

---
+++ ATMConsulting/app/src/main/java/com/khalilayache/atmconsulting/views/CompanyActivity.kt
@@ -0,0 +1,14 @@
+package com.khalilayache.atmconsulting.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.khalilayache.atmconsulting.R
+
+class CompanyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_company)
+    }
+}
