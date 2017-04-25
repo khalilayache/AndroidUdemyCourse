0e63c9c993e4a3d0c394f59c49d24b1987ef0041
Khalil Ayache <khalilayache@gmail.com>
2017-04-25 17:13:08 -0300

    Section 10, Class 78

--- CoinToss/app/src/main/java/com/khalilayache/cointoss/DetailsActivity.kt
+++ CoinToss/app/src/main/java/com/khalilayache/cointoss/DetailsActivity.kt
@@ -1,12 +1,44 @@
 package com.khalilayache.cointoss
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
 
 class DetailsActivity : AppCompatActivity() {
+
+    val NUMBER_SORTED = "NUMBER_SORTED"
+
+    private val backImageView: ImageView by lazy { findViewById(R.id.backImageView) as ImageView }
+    private val coinImageView: ImageView by lazy { findViewById(R.id.coinImageView) as ImageView }
+    val coinToss: Array<Int> = arrayOf(R.drawable.moeda_cara, R.drawable.moeda_coroa)
+
+    var numSorted: Int = 0
+
+    companion object{
+        fun start(context: Context, num: Int){
+            val intent = Intent(context,DetailsActivity::class.java)
+                    .putExtra("NUMBER_SORTED", num)
+            context.startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        initListener()
+        initView()
+    }
+
+    private fun initView() {
+        numSorted = intent.getIntExtra("NUMBER_SORTED", 0)
+        coinImageView.setImageResource(coinToss[numSorted])
+    }
+
+    private fun initListener() {
+        backImageView.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- CoinToss/app/src/main/java/com/khalilayache/cointoss/MainActivity.kt
+++ CoinToss/app/src/main/java/com/khalilayache/cointoss/MainActivity.kt
@@ -2,11 +2,23 @@ package com.khalilayache.cointoss
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val playImageView: ImageView by lazy { findViewById(R.id.playImageView) as ImageView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initListener()
+    }
+
+    private fun initListener() {
+        playImageView.setOnClickListener{
+            val numSorted = Random().nextInt(2)
+            DetailsActivity.start(this,numSorted)
+        }
     }
 }
